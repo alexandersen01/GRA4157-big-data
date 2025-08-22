a40634d9c21eac80f4ecb33f0afa47ca84e78688
Jakob Alexandersen <ja.al@fsncapital.com>
2025-08-22 14:36:05 +0200

    o2

--- exercises/ch_2/ball_table1.py
+++ exercises/ch_2/ball_table1.py
@@ -1,6 +1,13 @@
-from coor import gen_spaced_points
 
+g = 9.81
 # print(gen_spaced_points(0, 10, 40))
 
 def y(v_zero, t):
-    return v_zero
+    return v_zero - (0.5 * g * t**2)
+
+a = 0
+h = (b - a) / n
+
+# res = []
+# for i in range(n):
+#     res.append(a + i * h)
---
+++ exercises/ch_4/ball_cml_ValueError.py
@@ -0,0 +1,12 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="Calculate the height of a ball thrown upwards.")
+parser.add_argument('--v0', type=float, required=True, help='init v')
+parser.add_argument('--t', type=float, required=True, help='time')
+args = parser.parse_args()
+v0 = args.v0
+g = 9.81
+t = args.t
+y = v0*t - 0.5*g*t**2
+
+raise ValueError(f'bad interval cuh') if not 0 < y < (2 * (v0/g)) else print(y)
